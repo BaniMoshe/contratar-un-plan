39cf17e6f77b41fba0d6a422443772c3d945d3f7
Bani Moshe <banibarranco@gmail.com>
2022-02-05 22:42:04 -0500

    Bootstrap & local styles + scripts added!

--- banimoshe-bootstrap5/functions.php
+++ banimoshe-bootstrap5/functions.php
@@ -6,10 +6,14 @@ Theme functions
 
 function cwt_bootstrap5_enqueue_scripts() {
     #Adding styles to WP the old way -version sensitive args
-    wp_enqueue_style( 'style_cwtb5' , get_template_directory_uri() . '/style.css' , [ ] , filemtime( get_template_directory() . '/style.css' ) , 'all' );
-    #wp_enqueue_style( 'second_stylesheet' , get_template_directory_uri() . '/style.css' , [ 'style_cwtb5' ] ); #It first loads 'style_cwtb5'
+    wp_enqueue_style( 'stylesheet_cwtb5' , get_template_directory_uri() . '/assets/style.css' , 'bootstrap5_styles' , filemtime( get_template_directory() . '/assets/style.css' ) , 'all' );
+    wp_enqueue_style( 'bootstrap5_styles' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' , [] ); #This is loaded first
     #Or the easiest & quickest one
     #wp_enqueue_style( 'style_cwtb5' , get_stylesheet_uri() );
+    #And now the script
+    wp_enqueue_script( 'main_script_cwtb5' , get_template_directory_uri() . '/assets/main.js' );
+    wp_enqueue_script( 'bootstrap5_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js' );
+    wp_enqueue_script( 'bootstrap5_bundle_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' );
 }
 
 add_action( 'wp_enqueue_scripts' , 'cwt_bootstrap5_enqueue_scripts');
