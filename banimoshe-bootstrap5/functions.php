ce9059f3fab29af330d4743bfb92a9b64faeb790
Bani Moshe <banibarranco@gmail.com>
2022-02-07 00:33:31 -0500

    Register+enqueue were placed in order + Defined constants

--- banimoshe-bootstrap5/functions.php
+++ banimoshe-bootstrap5/functions.php
@@ -4,9 +4,13 @@ Theme functions file
 @package CWTBootstrap5
 */
 
-#Defining a constant to avoid naming the directory over and over again...
+#Defining constants to avoid naming the directory over and over again...
 if ( ! defined( 'CWTB5_DIR_PATH' ) ) {
     define( 'CWTB5_DIR_PATH' , untrailingslashit( get_template_directory() ) );
+}
+
+if ( ! defined( 'CWTB5_DIR_URI' ) ) {
+    define( 'CWTB5_DIR_URI' , untrailingslashit( get_template_directory_uri() ) );
 }
 
 #Requiring the autoloader
@@ -20,9 +24,9 @@ CWTB5_get_theme_instance();
 
 function cwt_bootstrap5_enqueue_scripts() {
     #Enqueuing styles to WP the old way with dependencies -version sensitive args
-    wp_enqueue_style( 'stylesheet_cwtb5' , get_template_directory_uri() . '/assets/style.css' , ['bootstrap5_styles'] , filemtime( CWTB5_DIR_PATH . '/assets/style.css' ) , 'all' );
+    #wp_enqueue_style( 'stylesheet_cwtb5' , CWTB5_DIR_URI . '/assets/style.css' , ['bootstrap5_styles'] , filemtime( CWTB5_DIR_PATH . '/assets/style.css' ) , 'all' );
     #Let's enqueue the Bootstrap5 styles
-    wp_enqueue_style( 'bootstrap5_styles' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' ); #This is loaded first
+    #wp_enqueue_style( 'bootstrap5_styles' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' ); #This is loaded first
     
     #QUICK NOTE:
         #Or you could've taken the easiest & quickest way
@@ -30,16 +34,16 @@ function cwt_bootstrap5_enqueue_scripts() {
 
     #QUICK NOTE:
         #But we could also register its name and under a certain condition either load a style or another. This is cool:
-        #wp_register_style( 'style_cwtb5' , get_template_directory_uri() . '/assets/style.css' , [] , filemtime( CWTB5_DIR_PATH . '/assets/style.css' ) , 'all' );
+        #wp_register_style( 'style_cwtb5' , CWTB5_DIR_URI . '/assets/style.css' , [] , filemtime( CWTB5_DIR_PATH . '/assets/style.css' ) , 'all' );
         #if ( $condition ) {
         #   wp_enqueue_style( 'style_cwtb5' );    #The same can be done with the JS files
         #}
 
     #And now the script with a dependency, just like the first enqueue
-    wp_enqueue_script( 'main_script_cwtb5' , get_template_directory_uri() . '/assets/main.js' , [ 'bootstrap5_min_js' ] , filemtime( CWTB5_DIR_PATH . '/assets/main.js' ) , 'false' );
+    #wp_enqueue_script( 'main_script_cwtb5' , CWTB5_DIR_URI . '/assets/main.js' , [ 'bootstrap5_min_js' ] , filemtime( CWTB5_DIR_PATH . '/assets/main.js' ) , 'false' );
     #Let's enqueue the Bootstrap5 scripts
-    wp_enqueue_script( 'bootstrap5_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js' ); #This is loaded first
-    wp_enqueue_script( 'bootstrap5_bundle_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' ); #This is loaded lastly
+    #wp_enqueue_script( 'bootstrap5_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js' ); #This is loaded first
+    #wp_enqueue_script( 'bootstrap5_bundle_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' ); #This is loaded lastly
 }
 
-add_action( 'wp_enqueue_scripts' , 'cwt_bootstrap5_enqueue_scripts');
+#add_action( 'wp_enqueue_scripts' , 'cwt_bootstrap5_enqueue_scripts' ] );
--- banimoshe-bootstrap5/inc/classes/class-cwtb5-theme.php
+++ banimoshe-bootstrap5/inc/classes/class-cwtb5-theme.php
@@ -13,10 +13,30 @@ class CWTB5_THEME {
 
     protected function __construct() {
         #We can load classes here
-        $this->set_hooks();
+        $this->setup_hooks();
     }
 
-    protected function set_hooks() {
-        #Actions and filters will be placed here
+    protected function setup_hooks() {
+        #Actions
+        add_action( 'wp_enqueue_scripts' , [ $this , 'register_styles' ] );
+        add_action( 'wp_enqueue_scripts' , [ $this , 'register_scripts' ] );
+    }
+
+    public function register_styles() {
+        #Styles registration
+        wp_register_style( 'stylesheet_cwtb5' , CWTB5_DIR_URI . '/assets/style.css' , ['bootstrap5_styles'] , filemtime( CWTB5_DIR_PATH . '/assets/style.css' ) , 'all' );
+        wp_register_style( 'bootstrap5_styles' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' );
+        wp_enqueue_style( 'stylesheet_cwtb5' );
+        wp_enqueue_style( 'bootstrap5_styles' );
+    }
+
+    public function register_scripts() {
+        #Scripts
+        wp_register_script( 'main_script_cwtb5' , CWTB5_DIR_URI . '/assets/main.js' , [ 'bootstrap5_min_js' ] , filemtime( CWTB5_DIR_PATH . '/assets/main.js' ) , 'false' );
+        wp_register_script( 'bootstrap5_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js' );
+        wp_register_script( 'bootstrap5_bundle_min_js' , 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' );
+        wp_enqueue_script( 'main_script_cwtb5' );
+        wp_enqueue_script( 'bootstrap5_min_js' );
+        wp_enqueue_script( 'bootstrap5_bundle_min_js' );
     }
 }
